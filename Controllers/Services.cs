538a3a54e47877b77e23b50a26706f690a6a120e
Atul Kumar <Atul.Kumar@elait.com>
2022-03-25 16:07:36 +0530

    First Commit Test Successful.

---
+++ Controllers/Services.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebsite.Controllers
+{
+    public class Services : Controller
+    {
+        public IActionResult TextAnnotation()
+        {
+            return View();
+        }
+        public IActionResult AudioTransciption()
+        {
+            return View();
+        }
+        public IActionResult ImageAnnotation()
+        {
+            return View();
+        }
+        public IActionResult ContentModeration()
+        {
+            return View();
+        }
+        public IActionResult ProductCategorization()
+        {
+            return View();
+        }
+        public IActionResult SentimentAnalysis()
+        {
+            return View();
+        }
+    }
+}
